2608b4a11bf870296912bdac483f3e7382b5d6be
Дмитрий Соломонов <solomon89@list.ru>
2020-11-27 23:51:21 +0300

    add db

--- app/googleFit.py
+++ app/googleFit.py
@@ -1,8 +1,10 @@
 from stavteam import app
 from flask import render_template
 from flask import jsonify, request
-
+from app import db
 
 @app.route('/auth2')
 def Auth():
-    return "<h1 style='color:orange'>Hello Google!</h1>"   
+    s = (db.GetAuth())
+    return "<h1 style='color:orange'> "+str(s)+" </h1>"   
+
