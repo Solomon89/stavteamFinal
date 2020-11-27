bc5a69da34e8e8082bf29ed7466650ecd850cd41
Дмитрий Соломонов <solomon89@list.ru>
2020-11-28 01:06:49 +0300

    tokens

--- app/googleFit.py
+++ app/googleFit.py
@@ -21,9 +21,29 @@ def login():
 
 def get_google_provider_cfg():
     return requests.get("https://accounts.google.com/.well-known/openid-configuration").json()
+
 @app.route('/auth/callback')
 def callback():
     # Get authorization code Google sent back to you
     code = request.args.get("code")
-    SaveAuth(code)
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg["token_endpoint"]
+    token_url, headers, body = client.prepare_token_request(
+        token_endpoint,
+        authorization_response=request.url,
+        redirect_url=request.base_url,
+        code=code
+    )
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=("473752170137-lu6o3d86puh7i6f8v5a1h93apv1evbmt.apps.googleusercontent.com", "4vxLyDnY7ylkd1F6Q8-5JvaD"),
+    )
+    # Parse the tokens!
+    client.parse_request_body_response(json.dumps(token_response.json()))
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+    SaveAuth(str(userinfo_response))
     return redirect(url_for('/'))
