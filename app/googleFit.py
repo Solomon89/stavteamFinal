b18c1ae18a049fe3cfb41ab1ca236c59fdb8bdab
Дмитрий Соломонов <solomon89@list.ru>
2020-11-28 00:47:10 +0300

    авторизация через google

--- app/googleFit.py
+++ app/googleFit.py
@@ -1,10 +1,23 @@
-from stavteam import app
+from stavteam import app,client
 from flask import render_template
-from flask import jsonify, request
+from flask import Flask, redirect, request, url_for
 from app import db
+import requests
 
-@app.route('/auth2')
-def Auth():
-    s = (db.GetAuth())
-    return "<h1 style='color:orange'> "+str(s)+" </h1>"   
+@app.route('/auth')
+def login():
+    # Find out what URL to hit for Google login
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
 
+    # Use library to construct the request for Google login and provide
+    # scopes that let you retrieve user's profile from Google
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
+    )
+    return redirect(request_uri)
+
+def get_google_provider_cfg():
+    return requests.get("https://accounts.google.com/.well-known/openid-configuration").json()
--- stavteam.py
+++ stavteam.py
@@ -1,9 +1,20 @@
 from flask import Flask
+import os
+
+from oauthlib.oauth2 import WebApplicationClient
+import requests
+
+# Configuration
+
+
 
 
 app = Flask(__name__)
 
+app.secret_key = "4vxLyDnY7ylkd1F6Q8-5JvaD"
 
+
+client = WebApplicationClient("473752170137-lu6o3d86puh7i6f8v5a1h93apv1evbmt.apps.googleusercontent.com")
 
 from app import views
 
