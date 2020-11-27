19b65c7bc48fa8c3440fe076d0a1920483e2b421
Дмитрий Соломонов <solomon89@list.ru>
2020-11-27 22:32:58 +0300

    add googleFit

---
+++ googleFit.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+
+@app.route("/Auth")
+def Auth():
+    return "<h1 style='color:orange'>Hello Google!</h1>"   
+
+
--- stavteam.py
+++ stavteam.py
@@ -5,5 +5,8 @@ app = Flask(__name__)
 def hello():
     return "<h1 style='color:orange'>Hello There!</h1>"
 
+
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=4345)
