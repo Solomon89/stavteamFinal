472c8c34fe7b7cd37ce1310849ec39b2e4636d97
Gennady <kukuruz-gs@mail.ru>
2020-11-28 19:22:28 +0300

    Страница пациента

---
+++ vue-project/src/components/pacient/Pacient.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div class="pacient-block">
+            Инфо о пациенте
+        </div>
+    </div>
+</template>
+
+<script>
+import AppHeader from '../main/Header.vue';
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+    components: {
+        AppHeader
+    },
+    methods: {
+
+    },
+    mounted() {
+        let data = {
+            session: sessionStorage.getItem('sessionId')
+        };
+    }
+}
+</script>
