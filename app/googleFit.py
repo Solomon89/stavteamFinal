432c06102c94390f1ece069785526118e1b3d86f
Tema <brykov93@gmail.com>
2020-11-28 19:43:37 +0300

    fitness make

--- app/googleFit.py
+++ app/googleFit.py
@@ -163,11 +163,11 @@ def getSteps(id):
         steps.append(value)
 
     fig, ax = plt.subplots()
-    ax.bar(days, steps)
+    ax.bar(days, steps,width=1)
     ax.set_facecolor('seashell')
     fig.set_facecolor('floralwhite')
-    fig.set_figwidth(5)  # ширина Figure
+    fig.set_figwidth(12)  # ширина Figure
     fig.set_figheight(6)  # высота Figure
-
+    ax.
     return fig_to_html(fig)
     
