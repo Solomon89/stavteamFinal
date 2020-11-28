11da81b42556bdcf83e4fb9c5c366ea603008994
Дмитрий Соломонов <solomon89@list.ru>
2020-11-28 11:50:34 +0300

    a

--- app/googleFit.py
+++ app/googleFit.py
@@ -18,7 +18,7 @@ def auth():
     request_uri = client.prepare_request_uri(
         authorization_endpoint,
         redirect_uri=request.base_url + "/callback",
-        scope=["openid", "email", "profile"],
+        scope=["openid", "email", "profile",'https://www.googleapis.com/auth/fitness.heart_rate.read'],
     )
     print(request_uri)
     return redirect(request_uri)
